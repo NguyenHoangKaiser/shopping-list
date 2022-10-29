7f0937fe568ceb668502b1b0b3224d3e8176be48
HuyHoang <dragonhuyhoang2001@gmail.com>
2022-10-29 13:45:09 +0700

    Initial Commit & itemRouter

---
+++ src/server/trpc/router/_app.ts
@@ -0,0 +1,10 @@
+// src/server/router/_app.ts
+import { router } from "../trpc";
+import { itemRouter } from "./itemRouter";
+
+export const appRouter = router({
+  items: itemRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
---
+++ src/server/trpc/trpc.ts
@@ -0,0 +1,14 @@
+import { initTRPC } from "@trpc/server";
+import type { Context } from "./context";
+import superjson from "superjson";
+
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+  errorFormatter({ shape }) {
+    return shape;
+  },
+});
+
+// Base router and procedure helpers
+export const router = t.router;
+export const publicProcedure = t.procedure;
